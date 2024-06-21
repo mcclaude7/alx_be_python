d2d77e9435ec60d452cdedc1a81f53bf89c975d3
unknown <mcclaude7>
2024-06-21 15:28:37 +0200

    updated future_age_calculator.py file in repo

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
-age = int(input("How old are you?: "))
+age = input("How old are you?: ")
 years_difference = 2050 - 2023
-age_expactation = age + years_difference
+age_expactation = int(age) + years_difference
 
 print("In 2050, you will be ",age_expactation," years old")
