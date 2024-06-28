ae9a4bde4b32ed2590054457c78788b52e191f2d
unknown <mcclaude7>
2024-06-28 15:47:13 +0200

    added pattern_drawing.py in repo

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+x = int(input("Enter the size of the pattern:"))
+row =0
+while (row<x):
+    for i in range(x):
+        print("*", end = "")
+              
+    print()
+    row += 1
+              
