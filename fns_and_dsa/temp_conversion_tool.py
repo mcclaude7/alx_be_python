16e695cd80f8b18c4b3c0a86a8be29ff66a843ba
unknown <mcclaude7>
2024-07-08 14:49:36 +0200

    Collected global variable of temp_conversion_tool.py to repository

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,11 +1,11 @@
-FAHRENHEIT_TO_CELISIUS_FACTOR = 5/9
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR ) + 32
 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELISIUS_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def main():
     temp = float(input("Enter the temperature to convert: "))
