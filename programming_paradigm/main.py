518d7ae2ad8c27fd80797674899ba2f0ee1ad2e5
unknown <mcclaude7>
2024-07-17 16:10:59 +0200

    Collected robust_division_calculator.py aand main file in repo

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -9,7 +9,8 @@ def main():
     numerator = sys.argv[1]
     denominator = sys.argv[2]
     result = safe_divide(numerator, denominator)
-    print(f"The result of the division is {result}")
+    print(f"{result}")
+    
 
 if __name__ == "__main__":
     main()
--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,11 +1,17 @@
+
+
 def safe_divide(numerator, denominator):
     try:
+        # Convert inputs to float
         num = float(numerator)
-        den = float(denominator)
-        if den == 0:
-            return "Error: Cannot divide by zero."
-        return num / den
+        denom = float(denominator)
+        
+        # Perform division
+        result = num / denom
+        return f"The result of the division is {result}"
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Please enter numeric value only."
+        return "Error: Please enter numeric values only."
 
 
