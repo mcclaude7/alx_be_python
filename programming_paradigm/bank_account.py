09515840b78166092d0a459ca902ab8a86a26a21
unknown <mcclaude7>
2024-07-18 09:45:53 +0200

    Added bank_account and main-0 file in repo

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self,initial_balance = 0):
+
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+
+    def widthdraw(self, amount):
+        if self.account_balance > amount:
+            self.account_balance = self.account_balance - amount 
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current balnce: {self.account_balance: .2f}") 
+
+if __name__ == "__main__":
+    account = BankAccount(100)
+    account.deposit(50)
+    account.widthdraw(40)
+    account.display_balance()
+
+
+    
