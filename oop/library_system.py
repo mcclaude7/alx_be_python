b6095e067d03d1e9d23bfddad8c4e652eb682b76
unknown <mcclaude7>
2024-07-22 12:59:29 +0200

    Collected libray_system file in oop directory

--- oop/library_system.py
+++ oop/library_system.py
@@ -53,7 +53,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self) :
-        return f"{super().__str__()} EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -61,7 +61,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"{super().__str__()},PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
