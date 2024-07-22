e26e16b522334b17d05bf25391e99f58913e1416
unknown <mcclaude7>
2024-07-22 10:42:58 +0200

    Added library_system and _main file in repo

---
+++ oop/library_system.py
@@ -0,0 +1,42 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(self, title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, File Size: {self.file_size}kb"
+
+class PrintBook(Book):
+    def __init__(self, page_count, title, author):
+        super().__init__(self, title, author)
+        self.page_count = page_count
+    
+    def __str__(self):
+        return f"{super().__str__()}, {self.title} by {self.author}, Page count: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+    
+    def add_books(self, book):
+        self.books.append(book)
+    
+    def list_books(self):
+        for book in self.books:
+            print(book)
+            
+
+
+
+
+
+
+    
